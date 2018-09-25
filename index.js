f2e4487ab7004aa0645a31132ea747bd42558e06
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-25 01:06:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+var kittens = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendKitten(name) {
-  var kittens = ["Milo", "Otis", "Garfield"]
   kittens.push(name)
-  return ["Milo", "Otis", "Garfield", "Ralph"]
-  // return kittens
 }
 
 // console.log(destructivelyAppendKitten("Ralph"))
